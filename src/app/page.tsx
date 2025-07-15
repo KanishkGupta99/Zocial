0c703b23e4758504c797f2144b7efc13e1d48b06
Kanishk Gupta <kanishkgupta@Kanishks-MacBook-Air.local>
2025-07-15 20:06:32 +0530

    added sidebar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      Home Page content
+    <div >
+
     </div>
   );
 }
